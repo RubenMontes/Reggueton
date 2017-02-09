93e7d047e5ef27a76085155273a001d75ae15b87
RubenMontes <montesdeltororuben@gmail.com>
2017-02-09 19:10:10 +0000

    palo de picas

--- Reggueton/src/reggueton/Carta.java
+++ Reggueton/src/reggueton/Carta.java
@@ -80,4 +80,6 @@ public class Carta {
         }
     }*/
     
+    /*implementar el compareTo()*/
+    
 }
--- Reggueton/src/reggueton/Reggueton.java
+++ Reggueton/src/reggueton/Reggueton.java
@@ -16,6 +16,23 @@ public class Reggueton {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Carta crisa = new Carta("crisa",20,'c');
+        Carta AsDePicas= new Carta("As de picas",1,'\u2660');
+        Carta dosDePicas= new Carta("2 de picas",2,'\u2660');
+        Carta tresDePicas= new Carta("3 de picas",3,'\u2660');
+        Carta cuatroDePicas= new Carta("4 de picas",4,'\u2660');
+        Carta cincoDePicas= new Carta("5 de picas",5,'\u2660');
+        Carta seisDePicas= new Carta("6 de picas",6,'\u2660');
+        Carta sieteDePicas= new Carta("7 de picas",7,'\u2660');
+        Carta ochoDePicas= new Carta("8 de picas",8,'\u2660');
+        Carta nueveDePicas= new Carta("9 de picas",9,'\u2660');
+        Carta diezDePicas= new Carta("10 de picas",10,'\u2660');
+        Carta onceDePicas= new Carta("J de picas",11,'\u2660');
+        Carta doceDePicas= new Carta("Q de picas",12,'\u2660');
+        Carta treceDePicas= new Carta("K de picas",13,'\u2660');
+        
+        System.out.println(AsDePicas.getName()+ " "+ AsDePicas.getvalue()+ AsDePicas.getSuit());
     }
     
 }
